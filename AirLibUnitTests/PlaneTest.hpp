742fb826f8b4cd4791de93aea2db2f2194610981
Artem Kuznetcov <artem.o.kuznecov@gmail.com>
2020-05-12 12:43:48 +0300

    add unit test fro create plane

--- AirLibUnitTests/PlaneTest.hpp
+++ AirLibUnitTests/PlaneTest.hpp
@@ -4,6 +4,7 @@
 #define msr_AirLibUnitTests_PlaneTest_hpp
 
 #include "vehicles/plane/PlaneParamsFactory.hpp"
+#include "vehicles/plane/Plane.hpp"
 #include "TestBase.hpp"
 
 namespace msr {
@@ -13,11 +14,40 @@ namespace msr {
 		public:
 			virtual void run() override
 			{
+				std::unique_ptr<msr::airlib::Kinematics> kinematics;
+				std::unique_ptr<msr::airlib::Environment> environment;
+
+				Kinematics::State initial_kinematic_state = Kinematics::State::zero();;
+				kinematics.reset(new Kinematics(initial_kinematic_state));
+
+				Environment::State initial_environment;
+				environment.reset(new Environment(initial_environment));
+
+				std::vector<std::unique_ptr<msr::airlib::Plane>> planes;
+
+				std::vector<std::string> vehicle_names;
+				for (auto const& vehicle_setting_pair : AirSimSettings::singleton().vehicles)
+				{
+					const auto& vehicle_setting = *vehicle_setting_pair.second;
+					// some settings loading
+					std::string name = vehicle_setting.vehicle_name.c_str();
+					vehicle_names.emplace_back(name);
+				}
 				//Test PX4 based drones
-				auto pixhawk = PlaneParamsFactory::createConfig(AirSimSettings::singleton().getVehicleSetting("Pixhawk"),
+				for (auto name : vehicle_names)
+				{
+					auto vehicle_params_ = PlaneParamsFactory::createConfig(AirSimSettings::singleton().getVehicleSetting(name),
+						std::make_shared<SensorFactory>());
+					auto vehicle_api_ = vehicle_params_->createPlaneApi();
+					testAssert(vehicle_api_ != nullptr, "Couldn't get pixhawk controller"); 
+					std::unique_ptr<Plane> phys_vehicle = std::unique_ptr<Plane>(new Plane(vehicle_params_.get(), vehicle_api_.get(),
+						kinematics.get(), environment.get(), name));
+					planes.push_back(std::move(phys_vehicle));
+				}
+				/*auto pixhawk = PlaneParamsFactory::createConfig(AirSimSettings::singleton().getVehicleSetting("Pixhawk"),
 					std::make_shared<SensorFactory>());
 				auto api = pixhawk->createPlaneApi();
-				testAssert(api != nullptr, "Couldn't get pixhawk controller");
+				testAssert(api != nullptr, "Couldn't get pixhawk controller");*/
 
 				try {
 					std::this_thread::sleep_for(std::chrono::milliseconds(10));
